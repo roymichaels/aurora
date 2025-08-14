eb8d97353b25199d3f255424f1f634b93b415da3
roymichaels <RoyMichaelsDotCom@pm.me>
2025-08-15 00:18:17 +0700

    feat: add quick action registry

---
+++ src/components/navigation/quickActions.ts
@@ -0,0 +1,29 @@
+import { useSyncExternalStore } from "react";
+
+export interface QuickAction {
+  id: string;
+  label: string;
+  icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+  onClick: () => void;
+}
+
+const actions: QuickAction[] = [];
+const listeners = new Set<() => void>();
+
+export function registerQuickAction(action: QuickAction) {
+  actions.push(action);
+  listeners.forEach((l) => l());
+}
+
+function subscribe(listener: () => void) {
+  listeners.add(listener);
+  return () => listeners.delete(listener);
+}
+
+function getSnapshot() {
+  return actions;
+}
+
+export function useQuickActions() {
+  return useSyncExternalStore(subscribe, getSnapshot, getSnapshot);
+}
