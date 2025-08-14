260795ce569210cc71facfaee986217a4db3f118
roymichaels <RoyMichaelsDotCom@pm.me>
2025-08-15 04:11:02 +0700

    feat: add modal host

---
+++ src/state/ui.ts
@@ -0,0 +1,21 @@
+import { create } from "zustand";
+
+export type ModalId =
+  | "brain"
+  | "journal"
+  | "live"
+  | "analytics"
+  | "more";
+
+type UIState = {
+  activeModal: ModalId | null;
+  openModal: (id: ModalId) => void;
+  closeModal: () => void;
+};
+
+export const useUIStore = create<UIState>((set) => ({
+  activeModal: null,
+  openModal: (id) => set({ activeModal: id }),
+  closeModal: () => set({ activeModal: null }),
+}));
+
