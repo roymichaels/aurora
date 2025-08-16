9b809a4c75fad12928a8f6023e2888f6e63d88c6
roymichaels <RoyMichaelsDotCom@pm.me>
2025-08-16 23:41:09 +0700

    feat: add aurora sphere component

---
+++ src/components/avatar/AuroraSphere.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { AuroraSphere } from './AuroraSphere';
+
+export default {
+  title: 'Avatar/AuroraSphere',
+  component: AuroraSphere,
+};
+
+export const Default = () => <AuroraSphere />;
+export const Speaking = () => <AuroraSphere speaking />;
