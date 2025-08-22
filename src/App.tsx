35980c2ba7da2d0d05f288a3f4f202ae46d4701f
roymichaels <RoyMichaelsDotCom@pm.me>
2025-08-22 19:17:26 +0700

    feat: enable react-router v7 flags

--- src/App.tsx
+++ src/App.tsx
@@ -99,7 +99,7 @@ const App = () => {
         <Toaster />
         <Sonner />
         <TTSPill />
-        <BrowserRouter>
+        <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
           {appShell ? <AppRoutesWithShell /> : <LegacyRoutes />}
         </BrowserRouter>
       </TooltipProvider>
