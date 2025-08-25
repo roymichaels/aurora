a8aa2da77b500366bbc0fbfef35a981fa7b59875
roymichaels <RoyMichaelsDotCom@pm.me>
2025-08-26 04:21:23 +0700

    refactor: use views registry in ModalHost

--- src/components/modals/ModalHost.tsx
+++ src/components/modals/ModalHost.tsx
@@ -1,5 +1,3 @@
-import LiveFocusView from "@/components/live/LiveFocusView";
-import HypnoPanel from "@/components/hypno/HypnoPanel";
 import {
   Dialog,
   DialogContent,
@@ -10,13 +8,10 @@ import {
 import { useChatInputFocus } from "@/hooks/useChatInputFocus";
 import { useUIStore } from "@/state/ui";
 import { lazy, Suspense, useEffect, type ReactNode } from "react";
-import AnalyticsModal from "@/components/modals/AnalyticsModal";
-import GoalsModal from "@/components/modals/GoalsModal";
-import SettingsModal from "@/components/modals/SettingsModal";
-import TasksModal from "@/components/modals/TasksModal";
 import { AuroraSphere } from "@/components/avatar/AuroraSphere";
 import { Button } from "@/components/ui/button";
 import { X } from "lucide-react";
+import { views } from "@/views/registry";
 
 const BrainView = views.find((v) => v.id === "brain")!.component;
 const FocusView = views.find((v) => v.id === "focus")!.component;
@@ -31,11 +26,6 @@ const SettingsModal = lazy(() => import("./SettingsModal"));
 const TasksModal = lazy(() => import("./TasksModal"));
 const OnboardingSheet = lazy(() => import("./OnboardingSheet"));
 const ConfirmSheet = lazy(() => import("./ConfirmSheet"));
-
-const BrainView = lazy(() => import("@/views/BrainView"));
-const JournalView = lazy(() => import("@/views/JournalView"));
-const FocusView = lazy(() => import("@/views/FocusView"));
-const VoiceView = lazy(() => import("@/views/VoiceView"));
 
 export default function ModalHost() {
   const { activeModal, closeModal, modalArgs } = useUIStore();
